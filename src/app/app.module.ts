8be16410322b72454ea0837a08ace41321cd4934
robbercs <roberto.calderon.sequera@alumnos.upm.es>
2023-11-29 20:39:58 +0100

    Incluida la galería

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,22 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HistoryComponent } from './history/history.component';
 import { CaesarComponent } from './caesar/caesar.component';
-import { RsaComponent } from './rsa/rsa.component';
-import { DiffieHellmanComponent } from './diffie-hellman/diffie-hellman.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPageComponent } from './principal-page/principal-page.component';
 import { FooterComponent } from './footer/footer.component';
 import { ElgamalComponent } from './elgamal/elgamal.component';
-import { BabyGiantComponent } from './baby-giant/baby-giant.component';
-import { PohligComponent } from './pohlig/pohlig.component';
-import { MillerComponent } from './miller/miller.component';
 import { FormsModule } from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms';
 import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const appRoute: Routes = [
   {path: '', component: PrincipalPageComponent },
   {path: 'historia', component: HistoryComponent },
   {path: 'caesar', component: CaesarComponent },
   {path: 'elgamal', component: ElgamalComponent },
-  {path: 'babyGiant', component: BabyGiantComponent },
-  {path: 'pohlig', component: PohligComponent },
-  {path: 'miller', component: MillerComponent },
-  {path: 'rsa', component: RsaComponent },
   {path: 'contact', component: ContactComponent },
-
+  {path: 'gallery', component: GalleryComponent }
 ]
 
 @NgModule({
@@ -37,15 +29,11 @@ const appRoute: Routes = [
     SidebarComponent,
     HistoryComponent,
     CaesarComponent,
-    RsaComponent,
-    DiffieHellmanComponent,
     PrincipalPageComponent,
     FooterComponent,
     ElgamalComponent,
-    BabyGiantComponent,
-    PohligComponent,
-    MillerComponent,
-    ContactComponent
+    ContactComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
